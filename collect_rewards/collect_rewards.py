cece546dbe82f19d69d1459d4f863d8c621a6a2e
yosupc <yosupche49@gmail.com>
2021-01-06 01:47:08 -0800

    set up background and a character on the screen

--- collect_rewards/collect_rewards.py
+++ collect_rewards/collect_rewards.py
@@ -2,17 +2,32 @@ import pygame
 
 pygame.init()
 
+# FPS
+clock = pygame.time.Clock()
+
+# set screen size
 screen_width = 640
 screen_height = 480
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-running = True
+# creating character's size
+character_width = 70
+character_height = 70
+x_pos = screen_width/2 - screen_width/2
+y_pos = screen_height - character_height
 
+running = True
 while running:
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill ((255, 0, 0))
+
+    
+
+    screen.fill ((100, 255, 100))
+    pygame.draw.rect(screen, (255, 100, 100),(x_pos, y_pos, character_width, character_height))
+
+
 
 pygame.quit()
